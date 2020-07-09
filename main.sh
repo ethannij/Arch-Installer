4daea604547137fba6716811fdc59eae8ad5580e
ethannij <67890942+ethannij@users.noreply.github.com>
2020-07-09 06:53:34 -0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -14,12 +14,12 @@ set -e
 
 #NameVM
 echo Enter the desired name for your VirtualBox ArchLinux
-read VMName
+read -r VMName
 VM=$VMName
 
 #Set Directory
 echo set VM directory. Prefferably you Virtualbox VMs folder
-read Path
+read -r Path
 Directory=$Path
 	mkdir "$Directory/$VMName"
 	cd "$Directory/$VMName"
@@ -27,48 +27,48 @@ Directory=$Path
 
 #creating virtual harddisk
 echo Input desired size of virtual harddisk MiB Min: 2000
-read diskSizeInput
+read -r diskSizeInput
 diskSize="$(( diskSizeInput ))"
 cd "$Directory/$VMName"
-VBoxManage createhd --filename $VM.vdi --size $diskSize
+VBoxManage createhd --filename "$VM".vdi --size $diskSize
 
 #Register VM
-VBoxManage createvm --name $VM --ostype "ArchLinux_64" --register
+VBoxManage createvm --name "$VM" --ostype "ArchLinux_64" --register
 
 #Adding VDI to VM
-VBoxManage storagectl $VM --name "SATA Controller" --add sata \
+VBoxManage storagectl "$VM" --name "SATA Controller" --add sata \
   --controller IntelAHCI
 
-VBoxManage storageattach $VM --storagectl "SATA Controller" --port 0 \
-  --device 0 --type hdd --medium $VM.vdi
+VBoxManage storageattach "$VM" --storagectl "SATA Controller" --port 0 \
+  --device 0 --type hdd --medium "$VM".vdi
 
 #Adding GuestAdditions to VM
 echo Input Guest Additions ISO directory
-read ADDDIR
+read -r ADDDIR
 ADD=$ADDDIR
 cd "$Directory"
 
-VBoxManage storagectl $VM --name "IDE Controller" --add ide
-VBoxManage storageattach $VM --storagectl "IDE Controller" --port 1 \
+VBoxManage storagectl "$VM" --name "IDE Controller" --add ide
+VBoxManage storageattach "$VM" --storagectl "IDE Controller" --port 1 \
  --device 1 --type dvddrive --medium "$ADD"
 
 #Adding ISO to VM
 echo Input Installation ISO directory
-read ISODIR
+read -r ISODIR
 ISO=$ISODIR
 cd "$Directory"
 
-VBoxManage storageattach $VM --storagectl "IDE Controller" --port 0 \
+VBoxManage storageattach "$VM" --storagectl "IDE Controller" --port 0 \
   --device 0 --type dvddrive --medium "$ISO"
 
 #Additional Configuration
-VBoxManage modifyvm $VM --ioapic on
-VBoxManage modifyvm $VM --boot1 dvd --boot2 disk --boot3 none --boot4 none
-VBoxManage modifyvm $VM --memory 1024 --vram 128
-VBoxManage modifyvm $VM --firmware efi
-VBoxManage modifyvm $VM --ioapic on
+VBoxManage modifyvm "$VM" --ioapic on
+VBoxManage modifyvm "$VM" --boot1 dvd --boot2 disk --boot3 none --boot4 none
+VBoxManage modifyvm "$VM" --memory 1024 --vram 128
+VBoxManage modifyvm "$VM" --firmware efi
+VBoxManage modifyvm "$VM" --ioapic on
  
 #Booting VM
-VBoxManage startvm $VM
+VBoxManage startvm "$VM"
 
 #Curl Install Script
