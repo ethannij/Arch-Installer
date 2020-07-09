04b7c8aead0d00f9e733c904c76087fa539c09fa
ethannij <67890942+ethannij@users.noreply.github.com>
2020-07-08 22:29:28 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -18,45 +18,38 @@ q
 EOF
 #
 #properly format main drive
-`mkfs.ext4 /dev/sda2`
+mkfs.ext4 /dev/sda2
 #properly format efi
-`mkfs.fat -F 32 /dev/sda1`
+mkfs.fat -F 32 /dev/sda1
 #mount boot drive
-`mount /dev/sda2 /mnt`
+mount /dev/sda2 /mnt
 #install pacstrap
-`pacstrap /mnt base linux linux-firmware`
+pacstrap /mnt base linux linux-firmware
 #genfstab
-`genfstab -U /mnt >> /mnt/etc/fstab`
+genfstab -U /mnt >> /mnt/etc/fstab
 #chroot
-`arch-chroot /mnt`
+arch-chroot /mnt
 #update hostname file
-`echo host >> /etc/hostname`
+echo host >> /etc/hostname
 #update hosts file
-`echo 127.0.0.1 localhost \n
-::1 localhost \n
-127.0.1.1 host >> /etc/hosts`
+echo 127.0.0.1 localhost ::1 localhost 127.0.1.1 host >> /etc/hosts
 #update pacman
-`pacman -Syu`
-`\n`
+pacman -Syu --noconfirm
 #network manager
-`pacman -S networkmanager`
-`\n`
+pacman -S networkmanager --noconfirm
 #mkinitcpio
-`mkinitcpio -P`
+mkinitcpio -P
 #set root password
-`passwd`
-`root`
-`root`
+echo -e "root" | passwd root
 #set up grub
-`pacman -S grub efibootmgr`
-`\n`
-`mkdir /boot/efi`
-`mount /dev/sda1 /boot/efi`
-`grub-install --target=86x_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB`
-`grub-mkconfig -o /boot/grub/grub.cfg`
-`exit`
+pacman -S grub efibootmgr --noconfirm
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-install --target=86x_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
 echo upon next boot. systemctl start NetworkManager.service and systemctl enable NetworkManger.service
 read
 echo username: root, password: root
 read
-`reboot`
+reboot
